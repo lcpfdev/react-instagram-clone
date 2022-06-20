519089559ecdbc3ff7b7d456ff91e566d483c2fe
Luka <lukafrance@gmail.com>
2022-06-20 17:45:50 +0100

    comment change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+const  App = () => {
+
+  const [pictures, setPictures] = useState([]);
+ 
+  useEffect(() =>{ 
+    handleFetch();
+  }, []);
+
+  const handleFetch = async () => { 
+    const response = await fetch ('https://picsum.photos/v2/list')
+    // this converts from json 
+    const data = await response.json();
+    setPictures(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+  
+      {pictures.map((item, index)=>{
+        return(
+          <div>
+            <h2>{item.author}</h2>
+            <img className='imageContainer' src = {item.download_url} alt = "random"/>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
